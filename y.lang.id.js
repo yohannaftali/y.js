039a369fdf9245c576059b34ef3d61bc44dc9de7
Yohan Naftali <yohan.naftali@gmail.com>
2022-08-19 09:49:37 +0700

    Update y.lang.id.js
    update

--- y.lang.id.js
+++ y.lang.id.js
@@ -1,4 +1,26 @@
 (function (window, undefined) {
+    const yLang = {
+        confirmationDownload: 'Download ?',
+        confirmationSubmit: 'Are you sure to submit?',
+        confirmationRecordCreate: 'Are you sure to create new record?',
+        confirmationRecordEdit: 'Are you sure to edit record?',
+        filterApply: 'Filter by',
+        filterClear: 'Clear All Filters',
+        download: 'Download',
+        new: 'New',
+        open: 'Open',
+        recordClone: 'Clone',
+        
+        // Table Info
+        processingRecordNo_Of_ :'Processing... Record No %1 of %2',
+        showAll: 'Show All',
+        showMore_Row: 'Show More %1 Row',
+        showMore_Rows: 'Show More %1 Rows',
+        showRecordLength: 'Show %1 of %2',
+        
+        uploadCsv: 'Upload CSV'
+    }
+
     const yStr = function (string, param){
         string = typeof string !== 'undefined' ? string: false
         param = typeof param !== 'undefined' ? param : false
@@ -21,27 +43,7 @@
         }
         return res
     }
-
-    const yLang = {
-        confirmationDownload: 'Download ?',
-        confirmationSubmit: 'Are you sure to submit?',
-        confirmationRecordCreate: 'Are you sure to create new record?',
-        confirmationRecordEdit: 'Are you sure to edit record?',
-        filterApply: 'Filter by',
-        filterClear: 'Clear All Filters',
-        download: 'Download',
-        new: 'New',
-        open: 'Open',
-        recordClone: 'Clone',
-        processingRecordNo_Of_ :'Processing... Record No %1 of %2',
-        showAll: 'Show All',
-        showMore_Row: 'Show More %1 Row',
-        showMore_Rows: 'Show More %1 Rows',
-        showRecordLength: 'Show %1 of %2',
-        uploadCsv: 'Upload CSV'
-    }
-
     if (typeof window === 'object' && typeof window.document === 'object') {
 		window.yStr = yStr;
 	}
-})(window);
+})(window)
